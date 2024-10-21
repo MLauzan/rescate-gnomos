e36f57bcff0768de916e9812bb3ae5cb9201e41c
Mlauzan <mateolauzan@gmail.com>
2024-10-21 17:52:32 -0300

    reemplazo de herencia de clases

--- src/juego/Casa.java
+++ src/juego/Casa.java
@@ -2,10 +2,58 @@ package juego;
 
 import java.awt.Image;
 
-public class Casa extends Estructura {
+public class Casa {
+	private int x;
+	private int y;
+	private int alto;
+	private int ancho;
+	private Image imagen;
 
 	public Casa(int x, int y, int alto, int ancho, Image Imagen) {
-		super(x, y, alto, ancho, Imagen);
-		// TODO Auto-generated constructor stub
+		this.setX(x);
+		this.setY(y);
+		this.setAlto(alto);
+		this.setAncho(ancho);
+		this.setImagen(imagen);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAlto() {
+		return alto;
+	}
+
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public Image getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
 	}
 }
--- src/juego/Tortuga.java
+++ src/juego/Tortuga.java
@@ -2,11 +2,88 @@ package juego;
 
 import java.awt.Image;
 
-public class Tortuga extends Personaje {
+public class Tortuga {
+	private int x;
+	private int y;
+	private int alto;
+	private int ancho;
+	private int velocidad;
+	private int vida;
+	private int daño;
+	private Image imagen;
 
 	public Tortuga(int x, int y, int alto, int ancho, int velocidad, int vida, int daño, Image imagen) {
-		super(x, y, alto, ancho, velocidad, vida, daño, imagen);
-		// TODO Auto-generated constructor stub
+		this.setX(x);
+		this.setY(y);
+		this.setAlto(alto);
+		this.setAncho(ancho);
+		this.setVelocidad(velocidad);
+		this.setVida(vida);
+		this.setDaño(daño);
+		this.setImagen(imagen);
 	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAlto() {
+		return alto;
+	}
+
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public Image getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
+	}
+
+	public int getDaño() {
+		return daño;
+	}
+
+	public void setDaño(int daño) {
+		this.daño = daño;
+	}
 }
